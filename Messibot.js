22340ba7612f8a1dc189e96ce66fda53812da45e
Christian Hiscock <drbronygamer@gmail.com>
2018-04-14 22:52:16 -0400

    Added 8ball and embed.
    Extra Var aswell.

--- Messibot.js
+++ Messibot.js
@@ -3,21 +3,52 @@
 const Discord = require('discord.js');
 const config = require("./config.json");
 const client = new Discord.Client();
+var RichEmbed;
+var embed;
+var buffer = 0;
+var fortune = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most Likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+  ]
 
 client.on('ready', () => {
   console.log('Messibot is online and ready to go!');
 });
 
-
-
-// Bot Commands
+// Bot Info
 client.on('message', message => {
-  if (message.content === '-commands') {
+  if (message.content === '-info') {
     embed = new Discord.RichEmbed()
       .addField("Welcome to messibot :D")
       .setColor(0xf4c430)
       .setFooter("Your family tree lgbt")
       .setThumbnail(message.author.avatarURL)
+    }
+});
+
+// Gives user 8ball response
+client.on('message', message => {
+  if(message.content == '-8ball'){
+    message.channel.send(fortune[(Math.floor(Math.random()*20))]);
   }
 });
 
